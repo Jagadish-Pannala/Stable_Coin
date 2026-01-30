2ae03b31c553f5aac28a0c5c84663509d9b7d666
smdteja <smdedu2003@gmail.com>
2026-01-30 11:34:39 +0530

    update password endpoint updated

--- API_Layer/Interfaces/authentication.py
+++ API_Layer/Interfaces/authentication.py
@@ -24,7 +24,6 @@ class RegisterResponse(BaseModel):
 
 class UpdatePasswordRequest(BaseModel):
     email: str
-    old_password: str
     new_password: str
 
 class UpdatePasswordResponse(BaseModel):
