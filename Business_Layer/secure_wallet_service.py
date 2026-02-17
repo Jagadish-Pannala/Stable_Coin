7518e59975b260e55eee9561a6534b26aaec61f8
smdteja <smdedu2003@gmail.com>
2026-02-17 17:57:49 +0530

    on wallet creatin eth added successfully

--- Business_Layer/secure_wallet_service.py
+++ Business_Layer/secure_wallet_service.py
@@ -1,13 +1,25 @@
+from decimal import Decimal
 from fastapi import HTTPException
 from eth_account import Account
 from mnemonic import Mnemonic
+from Business_Layer.authentication_service import AuthenticationService
 from DataAccess_Layer.dao.authentication_dao import UserAuthDAO
 import json
 
+from DataAccess_Layer.dao.tenant_dao import TenantDAO
+from DataAccess_Layer.dao.wallet_dao import WalletDAO
+from utils.web3_client import Web3Client
+
+from web3 import Web3
+
 class SecureWalletManager:
     def __init__(self, db=None):
+        self.web3 = Web3Client().w3
         self.db = db
         self.user_dao = UserAuthDAO(self.db)
+        self.wallet_dao = WalletDAO(self.db)
+        self.tenant_dao = TenantDAO(self.db)
+        self.auth_dao = AuthenticationService(self.db)
 
     def create_wallet_with_mnemonic(self, request, strength=256):
         try:
@@ -39,8 +51,46 @@ class SecureWalletManager:
             encrypted_keystore = account.encrypt(user.password)
 
             keystore_json = json.dumps(encrypted_keystore)
+            
+            wallet_address = self.web3.to_checksum_address(account.address)
+            amount = Decimal(str(1))
+            main_wallet = self.user_dao.get_main_wallet_address(request.tenant_id)
+            if not main_wallet:
+                raise HTTPException(
+                    status_code=500,
+                    detail="Main wallet not found for tenant"
+                )
+            rpc = self.tenant_dao.get_rpc_by_tenant_id(request.tenant_id)
+            if not rpc:
+                raise HTTPException(
+                    status_code=500,
+                    detail="RPC URL not found for tenant"
+                )
+            print("Main wallet:", main_wallet)
+            if request.tenant_id == 2:
+                amount = Decimal(str(0.01))
+                # Connect to tenant RPC
+                web3_rpc = Web3(Web3.HTTPProvider(rpc))
 
-            self.user_dao.create_wallet_for_user(request.customer_id, request.tenant_id, account.address, keystore_json)
+                # Get main wallet ETH balance
+                balance_wei = web3_rpc.eth.get_balance(main_wallet)
+                balance_eth = Decimal(web3_rpc.from_wei(balance_wei, "ether"))
+
+                # Prevent central wallet going below 28 ETH
+                if balance_eth - amount < Decimal("28"):
+                    raise HTTPException(
+                        status_code=400,
+                        detail="Unable to create wallet: central wallet ETH balance too low"
+                    )
+            
+            self.user_dao.create_wallet_for_user(
+                request.customer_id,
+                request.tenant_id,
+                account.address,
+                keystore_json
+            )
+            self.auth_dao.add_eth_wallet_creation(account.address, amount, main_wallet,rpc,tenant_id=request.tenant_id)
+            
 
             return account.address
 
